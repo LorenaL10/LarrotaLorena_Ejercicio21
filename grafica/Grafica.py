ae83715c2cdd9cc5226b3604837e1d8ed07aa4ee
Default user <l.larrota10@uniandes.edu.co>
2019-10-22 18:38:05 +0000

    codigo y grafica de coseno

--- grafica/Grafica.py
+++ grafica/Grafica.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x=np.linspace(0, 2*np.pi, 100)
+
 plt.figure()
-plt.plot(x, np.sin(x))
-plt.savefig('seno.png')
+plt.plot(x, np.cos(x))
+plt.title("coseno(x)")
+plt.savefig('funcion.png')
